cacb37a90387e1673afab274b5484ebbec8eeb47
prchristie <patrick.christie.dev@gmail.com>
2021-08-10 11:56:27 +0000

    hmm

--- .github/bumpversion.sh
+++ .github/bumpversion.sh
@@ -1,7 +1,6 @@
 git config user.name "${GITHUB_ACTOR}"
 git config user.email "patrick.christie.dev@gmail.com"
 git config --global push.followTags true
-git remote set-url origin https://${GITHUB_ACTOR}:${{ secrets.GITHUB_TOKEN }}@github.com/${GITHUB_REPOSITORY}
 
 last_merged_branch=$(git log --merges -n 1)
 branch_name="${last_merged_branch##*$'\n'}"
