e718e42a3ee9a3db45d45af1249fc719a8264295
prchristie <patrick.christie.dev@gmail.com>
2021-08-10 11:45:02 +0000

    No longer pull

--- .github/bumpversion.sh
+++ .github/bumpversion.sh
@@ -1,5 +1,3 @@
-git pull origin develop
-
 last_merged_branch=$(git log --merges -n 1)
 branch_name="${last_merged_branch##*$'\n'}"
 
@@ -11,27 +9,3 @@ case "$branch_name" in
     *[MAJOR]* ) bump2version minor;;
     *[MAJOR]* ) bump2version patch;;
 esac
-
-# git_refs_url=$(jq .repository.git_refs_url $GITHUB_EVENT_PATH | tr -d '"' | sed 's/{\/sha}//g')
-
-# git_refs_response=$(
-# curl -s -X POST $git_refs_url \
-# -H "Authorization: token $GITHUB_TOKEN" \
-# -d @- << EOF
-
-# {
-#   "ref": "refs/tags/$new",
-#   "sha": "$commit"
-# }
-# EOF
-# )
-
-# git_ref_posted=$( echo "${git_refs_response}" | jq .ref | tr -d '"' )
-
-# echo "::debug::${git_refs_response}"
-# if [ "${git_ref_posted}" = "refs/tags/${new}" ]; then
-#   exit 0
-# else
-#   echo "::error::Tag was not created properly."
-#   exit 1
-# fi
