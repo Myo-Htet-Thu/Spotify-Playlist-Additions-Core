4c321652f935c6aa31017499af810219403758d8
prchristie <patrick.christie.dev@gmail.com>
2021-08-10 12:20:48 +0000

    Trying new token

--- .github/bumpversion.sh
+++ .github/bumpversion.sh
@@ -2,6 +2,7 @@ git config user.name "${GITHUB_ACTOR}"
 git config user.email "patrick.christie.dev@gmail.com"
 git config --global push.followTags true
 git remote set-url origin https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}
+echo https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}
 last_merged_branch=$(git log --merges -n 1)
 branch_name="${last_merged_branch##*$'\n'}"
 
